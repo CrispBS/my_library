a63a98e8ce7c2f14e58463c14605842e9eb36abc
Sergey <sputienko10@gmail.com>
2022-04-11 05:28:21 +0300

    add more

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,30 @@
-import {sum} from './modules/test.js';
+import Navigo from 'navigo';
 
-document.write(sum(3,5))
+const library = document.querySelector('.library');
+const book = document.querySelector('.book');
+const add = document.querySelector('.add');
+
+const router = new Navigo('/', {
+    hash: true, 
+});
+
+const closeAllPage = () => {
+    library.classList.add('hidden');
+    book.classList.add('hidden');
+    add.classList.add('hidden');  
+}
+
+router.on( {
+    '/': () => {
+        closeAllPage();
+        library.classList.remove('hidden');
+    },
+    'book': () => {
+        closeAllPage();
+        library.classList.remove('hidden');
+    },
+    'add': () => {
+        closeAllPage();
+        library.classList.remove('hidden');
+    }
+}).resolve();
